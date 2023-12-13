7bd27fd7ed20cab8af8f025a77c5698ada2c457b
taiatulislam <taiatulislamapon@gmail.com>
2023-12-13 19:02:44 +0600

    helmet add in home

--- src/Layout/Home/Home.jsx
+++ src/Layout/Home/Home.jsx
@@ -2,10 +2,14 @@ import Banner from "./Banner";
 import ContentCreator from "./ContentCreator";
 import Popular from "./Popular";
 import Winner from "./Winner";
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Home</title>
+            </Helmet>
             <Banner></Banner>
             <Popular></Popular>
             <Winner></Winner>
